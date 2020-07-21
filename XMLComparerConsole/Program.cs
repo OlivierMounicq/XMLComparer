dd7b9e0f0d51b44024062a65607e1629da0610ca
Olivier Mounicq <mounicq@gmail.com>
2020-07-21 15:33:15 +0200

    1st commit : init the project

---
+++ XMLComparerConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XMLComparerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
